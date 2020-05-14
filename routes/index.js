d8f638845fa466efaaed2071d91de4e3147028b7
Nve Hounkponou Orphée <norpheehounnkponou@gmail.com>
2020-05-14 15:43:02 +0100

    FEAT interface connexion et inscription

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if(req.cookies.auth){
+    res.render('home', { title: Easytax});
+  } else {
+    res.render('connexion', { title: 'Connexion | Easytax' });
+  }
+});
+
+router.get('/connexion', function(req, res, next) {
+  if(req.cookies.auth){
+    res.render('home', { title: Easytax});
+  } else {
+    res.render('connexion', { title: 'Connexion | Easytax' });
+  }
+});
+
+router.get('/inscription', function(req, res, next) {
+  if(req.cookies.auth){
+    res.render('home', { title: Easytax});
+  } else {
+    res.render('inscription', { title: 'Inscription | Easytax' });
+  }
 });
 
 module.exports = router;
