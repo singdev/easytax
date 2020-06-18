4c4f3193f1b181a847de24aba1fd673660339aa8
Nve Hounkponou Orphée <norpheehounnkponou@gmail.com>
2020-06-18 23:14:08 +0100

    feat bouton retour, situation fiscale

--- public/javascripts/step_bar.js
+++ public/javascripts/step_bar.js
@@ -1,7 +1,21 @@
 let _progressBases = document.querySelectorAll('.base');
 let _progressBaseLines = document.querySelectorAll('.bar');
 
+function reload(){
+    for (let i = 0; i < _progressBases.length; i++) {
+        _progressBases[i].classList.remove('base-done');
+        _progressBases[i].classList.remove('base-active');
+        if (i > 0) {
+            _progressBaseLines[i - 1].classList.remove('bar-active');
+            _progressBaseLines[i - 1].classList.remove('bar-active');
+        }
+    }
+}
+
 function updateProgressBar(index) {
+    console.log("Update progress");
+    reload();
+    console.log(index);
     for (let i = 0; i < index; i++) {
         _progressBases[i].classList.add('base-done');
         if (i > 0) {
@@ -12,5 +26,4 @@ function updateProgressBar(index) {
     if(index > 0){
         _progressBaseLines[index - 1].classList.add('bar-active');
     }
-
 }
--- public/javascripts/util/form_step_management.js
+++ public/javascripts/util/form_step_management.js
@@ -5,6 +5,7 @@ let _bases = document.querySelectorAll('.form-container > div');
 /** Questions */
 let questions = [];
 let currentQuestion = 0;
+let stepStack = [];
 let currentShowDiv = null;
 
 /** Gestion des bases impossables */
@@ -20,6 +21,16 @@ function nextBase() {
     }
 }
 
+function previousBase(newBase){
+    _currentBase = newBase;
+    if(_currentBase < 0){
+        _currentBase = 0;
+    }
+    hideAllBase();
+    showBase(_currentBase);
+    initQuestion(_currentBase);
+}
+
 function hideAllBase() {
     _bases.forEach(b => {
         b.classList.remove('current-base');
@@ -27,6 +38,8 @@ function hideAllBase() {
 }
 
 function showBase(index) {
+    console.log(index);
+    console.log(index);
     _bases[index].classList.add('current-base');
     updateProgressBar(index);
 }
@@ -45,20 +58,36 @@ function initQuestion(currentBaseIndex) {
 }
 
 function nextQuestion(value) {
+    console.log(stepStack);
+    stepStack.push({ current: currentQuestion, baseNumber: _currentBase});
     if (value.includes("oui") || value == "next") {
         currentQuestion++;
     } else if (value.includes("non")) {
         currentQuestion += 2;
     }
-    if (currentQuestion >= questions.length) {
+    setCurrentQuestion(currentQuestion);
+}
+
+function setCurrentQuestion(index){
+    if (index >= questions.length) {
         nextBase();
     } else {
         hideAllQuestionAndDiv();
-        showQuestionAndDiv(currentQuestion);
+        showQuestionAndDiv(index);
     }
 }
 
 function previousQuestion(){
+    const previous = stepStack.pop();
+    console.log(previous);
+    if(previous == null){
+        return;
+    }
+    currentQuestion = previous.current;
+    if(previous.baseNumber != _currentBase){
+        previousBase(previous.baseNumber);
+    }
+    setCurrentQuestion(currentQuestion);
 }
 
 function showQuestionAndDiv(index) {
