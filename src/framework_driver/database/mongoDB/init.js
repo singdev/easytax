9ccac0960f5b2061b191d715cd5c0de2614ad83d
Nve Hounkponou Orphée <norpheehounkponou@gmail.com>
2020-08-28 11:39:52 +0100

    feat Docker file

--- src/framework_driver/database/mongoDB/init.js
+++ src/framework_driver/database/mongoDB/init.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
+const host = process.env.MONGO_HOST || 'localhost';
+
 /**
  * @param {String} dbName 
  */
 module.exports = (dbName) => {
-    mongoose.connect("mongodb://localhost:27017/" + dbName, {
+    mongoose.connect("mongodb://"+ host + ":27017/" + dbName, {
         useCreateIndex: true,
         useNewUrlParser: true, 
         useUnifiedTopology: true
