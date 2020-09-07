8d3f3c4a384c25d291e36e5796d11ffdef99ab9e
Nve Hounkponou Orphée <norpheehounkponou@gmail.com>
2020-09-07 15:37:28 +0100

    fix impression declaration irpp

--- public/javascripts/declaration/irpp.js
+++ public/javascripts/declaration/irpp.js
@@ -1,13 +1,13 @@
 function printIRPP() {
-    var printContents = document.getElementById('page-2').innerHTML;
+    var printContents = document.getElementById('fiche-irpp').innerHTML;
     var popupWin = window.open('', '_blank');
     popupWin.document.open();
     popupWin.document.write(`<html><head>
-        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css2?family=Jost&family=Noto+Sans&family=Noto+Sans+JP&family=Open+Sans&display=swap" />
-        <link rel="stylesheet" type="text/css" href="/stylesheets/page.css" />
-        <link rel="stylesheet" type="text/css" href="/stylesheets/style.css" />
-        <link rel="stylesheet" type="text/css" href="/stylesheets/declaration.css" />
-        <link rel="stylesheet" type="text/css" href="/stylesheets/declaration/is1_fiche.css" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Jost&family=Noto+Sans&family=Noto+Sans+JP&family=Open+Sans&display=swap" />
+        <link rel="stylesheet" href="/stylesheets/declaration.css" />
+        <link rel="stylesheet" href="/stylesheets/style.css" />
+        <link rel="stylesheet" href="/stylesheets/page.css" />
+        <link rel="stylesheet" href="/stylesheets/declaration/is1_fiche.css" />
         <link rel="stylesheet" type="text/css" href="/stylesheets/declaration/irpp_fiche.css" />
         </head><body onload="window.print()">` + printContents + '</body></html>');
     popupWin.document.close();
